92816f5462161c1145d4161467fb4cd9fea47ec9
Facundo <facundorullo132@gmail.com>
2026-05-28 23:14:14 -0300

    Clase 28/05

--- src/app/api/generate-note/route.js
+++ src/app/api/generate-note/route.js
@@ -3,33 +3,19 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
     
     try {
-        const {notes, messages} = await request.json()
 
-        const messagesFormateadas = messages
-        .map(m => `${m.role === "user" ? "Usuario:" : "Asistente IA"} : ${m.text}`)
-        .join("\n")
-
-        const notasFormateadas = notes
-        .map(n => `-Titulo: ${n.title}\n contenido: ${n.content}`)
-        .join("\n\n")
+        // tema "Reacrouter"
+        const { tema } = await request.json()
 
         const prompCompleto = `
-        Eres "Asistant IA" un modelo IA avanzado integrado en un sistema de notas y documentacion sobre desarrollo.
-        Responde de forma sumamente tecnia a las dudas del usuario.
-        Ve directo al grano.
-
-        Tienes acceso a la base de datos del usuario, debes priorizar esta informacion para responder.
-
-        NOTAS ACTUALES DEL USUARIO
-        ${notasFormateadas}
-
-        HISTORIAL DEL CHAT
-        ${messagesFormateadas}
-        
-        Responde al ultimo mensaje del usuario de forma conversacional.
-        Si usas codigo usa bloques de markdown
+        Genera una nota educativa sobre el siguiente tema: "${tema}"
+        Debes generar UNICAMENTE con un objeto JSON valido que contenga exactamente estas tres llaves (no agregues texto ni antes ni despues, solo el JSON): 
+        {
+            "title": "un titulo corto y profesional",
+            "content": "una explicacion conceptual breve en formato texto plano",
+            "ejemplo": "un bloque de codigo de ejemplo practico",
+        }
         `
-
         const apiKey = process.env.GEMINI_API_KEY
 
         if (!apiKey) {
@@ -37,7 +23,7 @@ export async function POST(request) {
         }
 
         // const url = `https://generativelanguage.googleapis.com/v1/models/gemini-3.5-flash:generateContent?key=${apiKey}`
-        const url = `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
+        const url = `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${apiKey}`
 
         const response = await fetch(url, {
                 method: "POST",
@@ -58,9 +44,13 @@ export async function POST(request) {
             )
         }
 
-        const aiText = data.candidates?.[0]?.content?.parts?.[0]?.text || "No pude generar respuesta"
+        const rawText = data.candidates?.[0]?.content?.parts?.[0]?.text || "{}"
+        const cleanJson = rawText.replace(/```json|```/g, "")
+        const noteData = JSON.parse(cleanJson)
 
-        return NextResponse.json({ success: true, result: aiText })
+        console.log(noteData)
+
+        return NextResponse.json({ success: true, result: noteData })
 
     } catch (err) {
         console.error(`Error critico en la ruta /api/ai ${err}`)
--- src/app/notes/create/page.jsx
+++ src/app/notes/create/page.jsx
@@ -4,12 +4,16 @@ import React from 'react'
 import { useState } from 'react'
 import { useNotes } from '../NotesContext'
 import { useRouter } from 'next/navigation'
+import axios from "axios"
 
 function CreateNotePage() {
   const router = useRouter()
   const { addNote, getDynamicCategories } = useNotes()
 
   const categorias = getDynamicCategories()
+
+  const [tema, setTema] = useState("")
+  const [loading, setLoading] = useState(false)
 
 
   const [formData, setFormData] = useState({
@@ -27,6 +31,28 @@ function CreateNotePage() {
     addNote(formData)
     router.push("/notes")
   }
+
+  const handleAutoFill = async (e) => {
+    e.preventDefault()
+    
+    if (!tema.trim() || loading) return
+    
+    setLoading(true)
+    try {
+      const response = await axios.post("/api/generate-note/", {tema})
+      setFormData({
+        title: response.data.result.title,
+        content: response.data.result.content,
+        ejemplo: response.data.result.content,
+      })
+    } catch (err) {
+      console.error(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+
   return (
     <section className='flex p-20 justify-center items-center w-full'>
       <form className="flex flex-col flex-1  p-6 rounded-lg bg-zinc-800 font-sans">
@@ -37,13 +63,34 @@ function CreateNotePage() {
 
         <p className="text-white text-lg font-semibold">Create Note</p>
 
+        <div className='mt-6 p-4 rounded border border-purple-500/30 gap-2 bg-zinc-900 flex flex-col'>
+          <label className='text-purple-400 text-xs font-bold tracking-wider'>Asisstant Copilot</label>
+          <div className='flex gap-2'>
+            <input 
+              type="text" 
+              placeholder='Ej: Arrow function en JS...'
+              className='flex-1 p-2 bg-zinc-800 rounded border border-zinc-700 focus:outline-none focus:border-purple-500' 
+              disabled={loading}
+              value={tema}
+              onChange={(e) => setTema(e.target.value)}
+            />
+            <button 
+              className={`bg-purple-600 hover:bg-purple-700 text-xs px-4 py-1 font-bold rounded disabled:opacity-50 cursor-pointer ${loading && "animate-pulse"}`}
+              onClick={handleAutoFill}
+              type='button'
+            >
+              {loading ? "Generando...": "Generar"}
+            </button>
+          </div>
+        </div>
+
         <div className='mt-10 flex flex-col gap-3'>
           <div className='flex flex-col'>
             <label className='text-zinc-400'>Title</label>
             <input 
               type="text" 
               placeholder='Title' 
-              className='p-2 border border-zinc-600 rounded-md my-4' 
+              className={`p-2 border border-zinc-600 rounded-md my-4 bg-zinc-900/80 focus:outline-none focus:border-purple-700 ${loading && "animate-pulse"}`}
               value={formData.title}
               onChange={(e) => setFormData({ ...formData, title: e.target.value })}
               />
@@ -52,7 +99,7 @@ function CreateNotePage() {
           <div className='flex flex-col'>
             <label className='text-zinc-400'>Category</label>
             <select 
-              className='p-2 border border-zinc-600 rounded-md my-4 cursor-pointer' 
+              className={`p-2 border border-zinc-600 bg-zinc-900/80 rounded-md my-4 cursor-pointer ${loading && "animate-pulse"}`}
               value={formData.category_id} 
               onChange={(e) => setFormData({ ...formData, category_id: String(e.target.value) })}
               >
@@ -66,7 +113,7 @@ function CreateNotePage() {
             <label className='text-zinc-400'>Content</label>
             <textarea 
               placeholder='Content' 
-              className='p-2 border border-zinc-600 rounded-md my-4' 
+              className={`p-2 border border-zinc-600 rounded-md my-4 bg-zinc-900/80 focus:outline-none focus:border-purple-700 ${loading && "animate-pulse"}`} 
               rows={10} 
               value={formData.content}
               onChange={(e) => setFormData({ ...formData, content: e.target.value })}
@@ -77,14 +124,21 @@ function CreateNotePage() {
             <label className='text-zinc-400'>Ejemplo</label>
             <textarea 
               placeholder='const variable' 
-              className='p-2 border border-zinc-600 rounded-md my-4' 
+              spellCheck={false}
+              className={`p-2 border border-zinc-600 rounded-md my-4 bg-zinc-950 font-mono focus:outline-none focus:border-purple-700 ${loading && "animate-pulse"}`} 
               rows={10} 
               value={formData.ejemplo}
               onChange={(e) => setFormData({ ...formData, ejemplo: e.target.value })}
             />
           </div>
           
-          <button onClick={handleSubmit} className='bg-blue-500 text-white p-2 rounded-md'>Save</button>
+          <button 
+            onClick={handleSubmit} 
+            type='submit' 
+            className='bg-blue-500 text-white p-2 rounded-md cursor-pointer'
+          >
+            Save
+          </button>
         </div>
 
       </form>
